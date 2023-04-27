43146a278d0eff7935a82f4525403d492fbbb9ec
Dhanashri-shelake <dhanashrishelake23@gmail.com>
2023-04-27 18:28:12 +0530

    helloo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('hello NARU');
+ res.send("hello NARU");
 });//listen to port 80 by default
 app.listen(process.env.PORT || 80);
 module.exports = app;
